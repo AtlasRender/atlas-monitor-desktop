3633fc65110cb57bee8e30bd79d38afe6145ff9c
ZaykaNya <48089558+ZaykaNya@users.noreply.github.com>
2021-02-24 23:31:32 +0200

    [GlOBAL] Eslint little changes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,25 @@
 module.exports = {
-  extends: "erb",
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint"
+  ],
+  extends: [
+    "erb",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
   rules: {
     // A temporary hack related to IDE not resolving correct package.json
     "import/no-extraneous-dependencies": "off",
+    "@typescript-eslint/no-namespace": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/ban-types": "off",
+    "only-arrow-functions": ["off"],
+    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    // "forin": false,  // TODO: check if there is a way to change this
+    "object-shorthand": "warn",
+    "no-console": "warn",
+    "no-shadow": ["error", {builtinGlobals: true}]
   },
   parserOptions: {
     ecmaVersion: 2020,
